e1d949337c5b52ae7ccc2c4c32b239bbd5028356
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-08-03 08:52:04 -0400

    setup vanilla-extract

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,17 @@ const Home: NextPage = () => {
   return (
     <>
       <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sequi,
+        aliquid, amet quae aliquam aut voluptate ullam dolor ex laboriosam nihil
+        maiores velit placeat quod, minima totam ipsam. Nesciunt, voluptates?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
+        perferendis ea libero, voluptatibus culpa praesentium nemo quisquam
+        asperiores, omnis provident cumque? Ut repellat dolores error? Neque
+        ducimus perferendis earum alias!
+      </p>
     </>
   );
 };
