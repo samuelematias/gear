e56f1bdf2f064d68e411de57a397728dc18f637f
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-08-07 15:44:34 -0400

    fix heading

--- components/MDXComponents/index.tsx
+++ components/MDXComponents/index.tsx
@@ -3,7 +3,12 @@ import { Gallery } from "../Gallery";
 import { Heading } from "../Heading";
 
 export const components = {
-  h2: (props) => <Heading {...props} />,
+  h2: ({ children }: { children: React.ReactNode }) => (
+    <Heading>{children}</Heading>
+  ),
+  h3: ({ children }: { children: React.ReactNode }) => (
+    <Heading as="h3">{children}</Heading>
+  ),
   Gallery,
   Profile,
 };
